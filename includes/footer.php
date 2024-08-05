2e882e492f4c00445456daa880031eb0bdd90e95
Abdullah Alfar <abdullah@wewebit.com>
2024-08-05 09:56:17 +0300

    init

--- includes/footer.php
+++
@@ -1,6 +0,0 @@
-    </main>
-    <footer>
-        <p>&copy; 2024 Your Name</p>
-    </footer>
-</body>
-</html>
--- index.php
+++
@@ -1,6 +0,0 @@
-<?php include 'includes/header.php'; ?>
-<section>
-    <h1>Welcome to My Portfolio</h1>
-    <p>Hello! I'm a PHP Developer. Check out my projects and get to know more about me.</p>
-</section>
-<?php include 'includes/footer.php'; ?>
